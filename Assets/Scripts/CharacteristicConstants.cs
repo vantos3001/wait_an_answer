5703621b1e6aaa446f1381d4f7e42af2678d28bc
Vano <iw.lasgin2011@gmail.com>
2019-07-16 21:18:07 +0300

    adjust balance

--- Assets/Scripts/CharacteristicConstants.cs
+++ Assets/Scripts/CharacteristicConstants.cs
@@ -3,27 +3,27 @@ namespace Game
     public class CharacteristicConstants
     {
         //When Eating
-        public static int EATING_DELTA_EAT = 5;
-        public static int EATING_DELTA_SLEEP = 5;
-        public static int EATING_DELTA_WORK = 5;
-        public static int EATING_DELTA_MOOD = 5;
+        public static int EATING_DELTA_EAT = 40;
+        public static int EATING_DELTA_SLEEP = -20;
+        public static int EATING_DELTA_WORK = -10;
+        public static int EATING_DELTA_MOOD = 0;
         
         //When Sleeping
-        public static int SLEEPING_DELTA_EAT = 5;
-        public static int SLEEPING_DELTA_SLEEP = 5;
-        public static int SLEEPING_DELTA_WORK = 5;
-        public static int SLEEPING_DELTA_MOOD = 5;
+        public static int SLEEPING_DELTA_EAT = -40;
+        public static int SLEEPING_DELTA_SLEEP = 60;
+        public static int SLEEPING_DELTA_WORK = -30;
+        public static int SLEEPING_DELTA_MOOD = 0;
         
         //When Working
-        public static int WORKING_DELTA_EAT = 5;
-        public static int WORKING_DELTA_SLEEP = 5;
-        public static int WORKING_DELTA_WORK = 5;
-        public static int WORKING_DELTA_MOOD = 5;
+        public static int WORKING_DELTA_EAT = -30;
+        public static int WORKING_DELTA_SLEEP = -30;
+        public static int WORKING_DELTA_WORK = 50;
+        public static int WORKING_DELTA_MOOD = 0;
         
         //When Relaxing
-        public static int RELAXING_DELTA_EAT = 5;
-        public static int RELAXING_DELTA_SLEEP = 5;
-        public static int RELAXING_DELTA_WORK = 5;
-        public static int RELAXING_DELTA_MOOD = 5;
+        public static int RELAXING_DELTA_EAT = -20;
+        public static int RELAXING_DELTA_SLEEP = -25;
+        public static int RELAXING_DELTA_WORK = -20;
+        public static int RELAXING_DELTA_MOOD = 20;
     }
 }
