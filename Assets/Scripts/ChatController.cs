683f66f1da5c407ea1ec6ca6a1cc522196596412
Vano <iw.lasgin2011@gmail.com>
2019-09-01 13:46:33 +0300

    add chat prototype

---
+++ Assets/Scripts/ChatController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Game.UI;
+using UnityEngine;
+
+namespace Game
+{
+    public class ChatController : MonoBehaviour
+    {
+        [SerializeField] private UIChatWindow _chatWindow;
+        
+            // Start is called before the first frame update
+            void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
+
+
---
+++ Assets/Scripts/UI/UIChatWindow.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Game.UI
+{
+    public class UIChatWindow : MonoBehaviour
+    {
+        [SerializeField] private Panel _namePanel;
+
+        [SerializeField] private Image _background;
+
+        [SerializeField] private GameObject _messagesGO;
+        
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+
+    public class UIChatElement : MonoBehaviour
+    {
+        
+    }
+
+}
+
+
